ee144f65cf856789816ee0abfa3e404734e58c73
AshRainK <kjo980430@gmail.com>
2023-07-12 14:53:34 +0900

    solve 11726

---
+++ baekjoon/src/DynamicProgramming/P_11726.java
@@ -0,0 +1,21 @@
+package DynamicProgramming;
+
+import java.io.*;
+
+public class P_11726 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        long dp[] = new long[n+2];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3; i<=n; i++){
+            dp[i] = (dp[i-1] + dp[i-2])%10007;
+        }
+        bw.write(dp[n]+"");
+        bw.flush();
+    }
+
+}
