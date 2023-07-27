0b09f9111b1a70786e48de6edc41714ef9ea1baf
AshRainK <kjo980430@gmail.com>
2023-07-27 19:58:16 +0900

    solve 9656

---
+++ baekjoon/src/DynamicProgramming/P_9656.java
@@ -0,0 +1,28 @@
+package DynamicProgramming;
+
+import java.io.*;
+
+public class P_9656 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int dp[] = new int[N+3];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 1;
+        for(int i = 4; i<= N; i++){
+            dp[i] = Math.min(dp[i-1]+1,dp[i-3]+1);
+        }
+        StringBuilder sb = new StringBuilder();
+        if(dp[N] % 2 == 0){
+            sb.append("SK");
+        }
+        else
+            sb.append("CY");
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+}
